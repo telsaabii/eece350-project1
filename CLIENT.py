00551fad8680cc990ab928b947c1d5f0a28d6d44
telsaabii <125814870+telsaabii@users.noreply.github.com>
2023-02-20 14:36:03 +0200

    Update CLIENT.py

--- CLIENT.py
+++ CLIENT.py
@@ -8,6 +8,7 @@ website = 'www.example.com'
 proxy_port = 8901
 proxy_host = str(socket.gethostbyname(socket.gethostname()))
 
+#socket the client will use to communicate with the proxy server
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((proxy_host,proxy_port))
 
@@ -15,11 +16,11 @@ time1 = time.time()
 
 #website= input("Enter the website ip you want to access: ")
 request = messagetosend=f"GET http://{website}/HTTP/1.1\r\nHost:{website}\r\n\r\n"
-client_socket.sendall(request.encode())
+client_socket.sendall(request.encode())#send the clients request to the proxy server which will process the request and send to main destination
 print("Requesting to go to: "+ website)
 
 #response from proxy server
-response = client_socket.recv(1024).decode()
+response = client_socket.recv(1024).decode()#receiving response from proxy server about the previous request
 time2=time.time()
 print("The response is: "+ response)
 
